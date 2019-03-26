21549dd7cb6cd3af70b5b98669ef7a2a091e0250
Alice Mingay <alice.mingay@gmail.com>
2019-03-26 17:17:01 +0000

    edited what R2R info displays on results.html page

--- travel_app.py
+++ travel_app.py
@@ -5,6 +5,7 @@ app = Flask("app")
 
 with open("credentials.txt","r") as file:
     R2R_API = file.readline().split()[2]
+    ZOMATO_API = file.readline().split()[2]
     MAILGUN_API = file.readline().split()[2]
     MAILGUN_DOMAIN_NAME = file.readline().split()[2]
 
@@ -25,21 +26,28 @@ def getRoute(startLocation, destination):
     payload = {"key": R2R_API, "oName": startLocation, "dName": destination}
     r = requests.get(endpoint, params=payload).json()
 
-    print r
 
     R2R_categories = {
-    "start_point" : r["places"][0]["shortName"],
-    "end_point" : r["places"][1]["shortName"],
+    "start_point" : r["places"][0]["longName"],
+    "end_point" : r["places"][1]["longName"],
     "route_0_name" : r["routes"][0]["name"],
     "route_0_arrPlace" : r["routes"][0]["arrPlace"],
     "route_0_depPlace" : r["routes"][0]["depPlace"],
+    "route_0_distance" : r["routes"][0]["distance"],
     "route_0_totalDuration" : r["routes"][0]["totalDuration"],
+    "route_0_url" : r["routes"][0]["segments"],
+    "route_0_currency" : r["routes"][0]["indicativePrices"][0]["nativeCurrency"],
+    "route_0_PriceHigh" : r["routes"][0]["indicativePrices"][0]["nativePriceHigh"],
+    "route_0_PriceLow" : r["routes"][0]["indicativePrices"][0]["nativePriceLow"],
     "route_0_full" : r["routes"][0],
-    "route_1" : r["routes"][1],
-    "route_2" : r["routes"][2],
     }
 
     return render_template("results.html", R2R_categories=R2R_categories)
+
+@app.route("/restaurants", methods=["GET"])
+def restaurants(London):
+
+    return render_template("results.html")
 
 #@app.route("/email", methods=["POST"])
 #def send_email():
